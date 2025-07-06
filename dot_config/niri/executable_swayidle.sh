fac813528e04839f6b2047649b9bc9b599b504d9
Иван Јањић <wannjanjic@gmail.com>
2025-07-06 14:54:32 +0200

    fix everything

--- dot_config/niri/executable_swayidle.sh
+++ dot_config/niri/executable_swayidle.sh
@@ -1,4 +1,4 @@
 #!/bin/sh 
-swayidle -w timeout 300 'swaylock -f -i ~/.wallpaper.png' timeout 600 'niri msg action power-off-monitors' before-sleep 'swaylock -f -i ~/.wallpaper.png'
+swayidle -w timeout 300 'swaylock' timeout 600 'niri msg action power-off-monitors' before-sleep 'swaylock'
 
 
--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -4,7 +4,7 @@ if status is-interactive
 end
 starship init fish | source
 
-set -Ux GEMINI_API_KEY "my_gemini_api_key"
+set -Ux GEMINI_API_KEY "AIzaSyAlUrkGpsTvqT9GQ8OteDYg4b1DLuY8DWM"
 # Add Go bin to PATH if not already present
 if not contains $HOME/go/bin $PATH
     set -Ua PATH $HOME/go/bin
