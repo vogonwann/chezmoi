088a55534a79f4acc967804c89e223ab7f82a7e1
Иван Јањић <wannjanjic@gmail.com>
2025-07-09 13:07:10 +0200

    add fastfetch config

--- dot_config/niri/executable_swayidle.sh
+++ dot_config/niri/executable_swayidle.sh
@@ -1,4 +1,4 @@
 #!/bin/sh 
-swayidle -w timeout 300 'swaylock' timeout 600 'niri msg action power-off-monitors' before-sleep 'swaylock'
+swayidle -w timeout 600 'swaylock' timeout 900 'niri msg action power-off-monitors' before-sleep 'swaylock'
 
 
